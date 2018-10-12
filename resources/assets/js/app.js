19ae05a05e2d053b4de1b46f2712f60b8f0362d4
scliftas <scliftas@gmail.com>
2018-10-12 10:19:41 +0100

    Added Bootstrap Vue, Hover.css and Lodash Node packages

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,9 +3,14 @@ import store from '~/store'
 import router from '~/router'
 import i18n from '~/plugins/i18n'
 import App from '~/components/App'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import '~/plugins'
 import '~/components'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
