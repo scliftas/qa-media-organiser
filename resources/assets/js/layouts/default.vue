296799c0a221333b8e0b50860446db4c16b94cf0
scliftas <scliftas@gmail.com>
2018-10-12 12:41:33 +0100

    Redesigned default layout

--- resources/assets/js/layouts/default.vue
+++ resources/assets/js/layouts/default.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="main-layout">
+  <div class="main-layout text-center">
+    <sidebar/>
     <navbar/>
 
-    <div class="container mt-4">
+    <div class="container-fluid mt-4">
       <child/>
     </div>
   </div>
 </template>
 
 <script>
+import Sidebar from '~/components/Sidebar'
 import Navbar from '~/components/Navbar'
 
 export default {
   name: 'MainLayout',
 
   components: {
+    Sidebar,
     Navbar
   }
 }
