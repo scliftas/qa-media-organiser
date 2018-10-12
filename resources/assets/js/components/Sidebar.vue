f2e76116e80fd6673aae2a6815c947ff532cf2de
scliftas <scliftas@gmail.com>
2018-10-12 12:41:11 +0100

    Added sidebar component

---
+++ resources/assets/js/components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <div id="sidebar">
+        <div class="sidebar-header p-4 text-info">
+            <router-link :to="'/'" class="h4">
+              {{ appName }}
+            </router-link>
+        </div>
+
+        <upload/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Upload from '~/components/upload'
+import axios from 'axios'
+
+export default {
+  data: () => ({
+    appName: window.config.appName,
+    nav_items: []
+  }),
+
+  components: {
+    Upload
+  },
+
+  computed: mapGetters({
+    user: 'auth/user',
+    typeId: 'files/typeId'
+  }),
+
+  mounted () {
+  },
+
+  methods: {
+  }
+}
+</script>
