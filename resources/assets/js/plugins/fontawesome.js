be30ebb247d66cd1ab312a0d112948b908a3a144
scliftas <scliftas@gmail.com>
2018-10-12 15:29:45 +0100

    Added image icon in font awesome plugin

--- resources/assets/js/plugins/fontawesome.js
+++ resources/assets/js/plugins/fontawesome.js
@@ -7,9 +7,9 @@ Vue.component('fa', FontAwesomeIcon)
 // import { } from '@fortawesome/fontawesome-free-regular/shakable.es'
 
 import {
-  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay
+  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage
 } from '@fortawesome/fontawesome-free-solid/shakable.es'
 
 fontawesome.library.add(
-  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay
+  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage
 )
