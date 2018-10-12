39abb3a63064b41532b050a29e7f7b31b7f66502
scliftas <scliftas@gmail.com>
2018-10-12 13:50:29 +0100

    Added files component to home page

--- resources/assets/js/pages/home.vue
+++ resources/assets/js/pages/home.vue
@@ -1,12 +1,20 @@
 <template>
+  <files/>
 </template>
 
 <script>
+import axios from 'axios'
+import Files from '~/components/Files'
+
 export default {
   middleware: 'auth',
 
+  components: {
+    Files
+  },
+
   metaInfo () {
-    return { title: this.$t('home') }
-  }
+    return { title: this.$t('Files') }
+  },
 }
 </script>
