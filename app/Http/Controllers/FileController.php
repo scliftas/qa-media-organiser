0289579bed4c080b0e9cbe115b4e6bdffab31074
scliftas <scliftas@gmail.com>
2018-10-12 12:34:57 +0100

    Added file controller class

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Services\FileService;
+use App\Requests\UploadFileRequest;
+use App\Resources\FileResource;
+
+class Fileontroller extends Controller
+{
+    protected $file_service;
+
+    public function __construct(FileService $file_service) {
+        $this->file_service = $file_service;
+    }
+
+    public function upload(UploadFileRequest $request) {
+        $uploaded_files = [];
+
+        foreach ($request->files as $files) {
+            foreach ($files as $file) {
+                array_push($uploaded_files, $this->file_service->create($file));
+            }
+        }
+
+        $uploaded_files = collect($uploaded_files);
+
+        return FileResource::collection($uploaded_files)->resolve();
+    }
+}
