16299b1e6c76149495cac95a145e36c9b3283cff
scliftas <scliftas@gmail.com>
2018-10-15 15:13:10 +0100

    Added use statement for Auth facade in category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Repositories\CategoryRepository;
 use Illuminate\Http\Request;
 use App\Http\Resources\CategoryResource;
+use Auth;
 
 class CategoryController extends Controller
 {
