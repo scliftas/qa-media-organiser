588acfb38972bc0cda08e7d7f636ab0850de27c0
scliftas <scliftas@gmail.com>
2018-10-15 17:35:19 +0100

    Added playlists to sidebar component

--- resources/assets/js/components/Sidebar.vue
+++ resources/assets/js/components/Sidebar.vue
@@ -12,7 +12,7 @@
               <span class="my-auto ml-3">All</span>
             </li>
             <dropdown title="Categories" :items="this.categories" @itemChosen="setCategory" @createNewItem="createNewCategory"/>
-            <dropdown title="Playlists" :items="[{ name: 'Test Playlist 1' }]" @itemChosen="setPlaylist"/>
+            <dropdown title="Playlists" :items="this.playlists" @itemChosen="setPlaylist" @createNewItem="createNewPlaylist"/>
         </ul>
 
         <upload/>
@@ -39,16 +39,19 @@ export default {
 
   computed: mapGetters({
     user: 'auth/user',
-    categories: 'categories/categories'
+    categories: 'categories/categories',
+    playlists: 'playlists/playlists'
   }),
 
   mounted () {
     this.$store.dispatch('categories/getCategories')
+    this.$store.dispatch('playlists/getPlaylists')
   },
 
   methods: {
     resetFilters () {
       this.$store.dispatch('categories/clearCurrentCategory')
+      this.$store.dispatch('playlists/clearCurrentPlaylist')
     },
 
     setCategory (category) {
@@ -56,11 +59,15 @@ export default {
     },
 
     setPlaylist (playlist) {
-      console.log(playlist)
+      this.$store.dispatch('playlists/setCurrentPlaylist', playlist)
     },
 
     createNewCategory () {
       this.$root.$emit('bv::show::modal', 'category-modal')
+    },
+
+    createNewPlaylist () {
+      this.$root.$emit('bv::show::modal', 'playlist-modal')
     }
   }
 }
