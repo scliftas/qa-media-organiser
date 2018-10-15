e588b4ad2ede8a50360b734d56eb4711bac77c45
scliftas <scliftas@gmail.com>
2018-10-15 17:19:39 +0100

    Added playlist resource class

---
+++ app/Http/Resources/PlaylistResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class PlaylistResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name
+        ];
+    }
+}
