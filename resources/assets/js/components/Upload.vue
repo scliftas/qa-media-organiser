c4cdf4d68404cab5a4cd2aa79acb57ef2339407c
scliftas <scliftas@gmail.com>
2018-10-16 16:40:02 +0100

    Upload file input now only accepts audio and video files

--- resources/assets/js/components/Upload.vue
+++ resources/assets/js/components/Upload.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="upload" class="text-white bg-success align-bottom p-4 d-flex justify-content-center hvr-icon-up">  
-        <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="save($event.target.files); fileCount = $event.target.files.length" class="input-file"> 
+        <input accept="video/*, audio/*" type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="save($event.target.files); fileCount = $event.target.files.length" class="input-file"> 
         <span class="my-auto h4 mr-3">Upload</span>
         <fa icon="cloud-upload-alt" class="upload-icon hvr-icon"></fa>
     </div>
