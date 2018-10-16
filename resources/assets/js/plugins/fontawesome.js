f441f3b23b7901702defe8c051711c95734dd7e7
scliftas <scliftas@gmail.com>
2018-10-16 12:25:14 +0100

    Added up arrow, down arrow and edit icons to font awesome plugin

--- resources/assets/js/plugins/fontawesome.js
+++ resources/assets/js/plugins/fontawesome.js
@@ -7,9 +7,9 @@ Vue.component('fa', FontAwesomeIcon)
 // import { } from '@fortawesome/fontawesome-free-regular/shakable.es'
 
 import {
-  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage, faCaretDown, faCaretUp, faPlus
+  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage, faCaretDown, faCaretUp, faPlus, faArrowUp, faArrowDown, faEdit
 } from '@fortawesome/fontawesome-free-solid/shakable.es'
 
 fontawesome.library.add(
-  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage, faCaretDown, faCaretUp, faPlus
+  faUser, faLock, faSignOutAlt, faCog, faCloudUploadAlt, faDownload, faTrash, faPlay, faImage, faCaretDown, faCaretUp, faPlus, faArrowUp, faArrowDown, faEdit
 )
