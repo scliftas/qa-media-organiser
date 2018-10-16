f940b0a5f5360e7f1443a85770470a44e7f3299d
scliftas <scliftas@gmail.com>
2018-10-16 15:46:37 +0100

    Added create from blob method to file repository class

--- app/Repositories/FileRepository.php
+++ app/Repositories/FileRepository.php
@@ -27,6 +27,25 @@ class FileRepository extends AbstractRepository {
         return false;
     }
 
+    public function createFromBlob($data) {
+        $file_name = $data->name . '.' . $data->type;
+
+        if (Storage::disk('local')->put($file_name, base64_decode($data->blob))) {
+            $file = [
+                'user_id' => Auth::user()->id,
+                'name' => $data->name,
+                'type' => $data->type,
+                'path' => Storage::disk('local')->path($file_name)
+            ];
+
+            if (property_exists($data, 'comment')) $file['comment'] = $data->comment;
+            
+            return $this->model->updateOrCreate($file);
+        }
+
+        return false;
+    }
+
     public function delete($id) {
         $file = parent::get($id);
         if (Storage::disk('local')->delete($file->name . '.' . $file->type)) return parent::delete($id);
