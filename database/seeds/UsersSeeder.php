4bbdef4b2c406c29fe33bf6f55a8625cd8069e80
scliftas <scliftas@gmail.com>
2018-10-16 15:59:27 +0100

    Added users table database seeder class

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'testuser@testemail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
