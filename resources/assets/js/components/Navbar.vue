f2d265fca8e2e70a0fa9a79fb92d156ac43725b9
scliftas <scliftas@gmail.com>
2018-10-16 14:33:11 +0100

    Save export response to file in navbar component

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -62,7 +62,18 @@ export default {
     },
 
     async generateExport () {
-      let response = await axios.post('/api/export/generate');
+      axios({
+        url: '/api/export/generate',
+        method: 'POST',
+        responseType: 'blob', // important
+      }).then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'export.json');
+        document.body.appendChild(link);
+        link.click();
+      });
     }
   }
 }
