5069d9d108650750ca26db0303d97580fe8086e8
scliftas <scliftas@gmail.com>
2018-10-16 17:38:29 +0100

    Added delete method to category repository class

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -9,4 +9,11 @@ class CategoryRepository extends AbstractRepository {
     public function __construct(Category $category) {
         parent::__construct($category);
     }   
+
+    public function delete($id) {
+        $category = $this->get($id);
+        $category->files()->detach();
+
+        return parent::delete($id);
+    }
 }
