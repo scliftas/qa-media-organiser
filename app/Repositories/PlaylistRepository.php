c8bf7f68592b711de01366dc8db511e6c3a19a0e
scliftas <scliftas@gmail.com>
2018-10-16 17:43:01 +0100

    Added delete method to playlist repository class

--- app/Repositories/PlaylistRepository.php
+++ app/Repositories/PlaylistRepository.php
@@ -9,4 +9,11 @@ class PlaylistRepository extends AbstractRepository {
     public function __construct(Playlist $category) {
         parent::__construct($category);
     }   
+
+    public function delete($id) {
+        $playlist = $this->get($id);
+        $playlist->files()->detach();
+
+        return parent::delete($id);
+    }
 }
