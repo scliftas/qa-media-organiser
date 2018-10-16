9605ef494072ca7eeff86ecb7695379a1f05de9c
scliftas <scliftas@gmail.com>
2018-10-16 12:25:38 +0100

    [WIP] Added bootstrap vue table to playlist component

--- resources/assets/js/components/Playlist.vue
+++ resources/assets/js/components/Playlist.vue
@@ -1,26 +1,31 @@
 <template>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Position</th>
-                <th scope="col">Name</th>
-                <th scope="col">Type</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(file, index) in files" :key="index">
-                <th scope="row">{{ index }}</th>
-                <td>{{ file.name }}</td>
-                <td>{{ file.type }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="w-100 p-3">
+        <b-table :fields="fields" :items="getFiles()" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :hover="true" class="text-left">
+            <template slot="actions" slot-scope="data">
+                <fa icon="arrow-up" class="text-success m-1" v-if="data.index > 0" v-b-tooltip.hover title="Move Up"></fa>
+                <fa icon="arrow-down" class="text-success m-1" v-b-tooltip.hover title="Move Down"></fa>
+                <fa icon="edit" class="text-success m-1" v-b-tooltip.hover title="Edit" @click="showModal(data.item)"></fa>
+            </template>
+        </b-table>
+    </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
+
+    data: () => ({
+        fields: [
+            { key: 'position', sortable: true, sortDirection: 'last' },
+            { key: 'name' },
+            { key: 'type' },
+            { key: 'actions' }
+        ],
+        sortDesc: false,
+        sortBy: 'position'
+    }),
+
     computed: mapGetters({
         files: 'files/files',
         currentPlaylist: 'playlists/currentPlaylist'
@@ -28,7 +33,29 @@ export default {
 
     methods: {
         showFile (file) {
-            return file.playlists.includes(this.currentPlaylist.id)
+            return Object.keys(file.playlists).map(Number).includes(this.currentPlaylist.id)
+        },
+
+        getPosition (file) {
+            return file.playlists[this.currentPlaylist.id][0].pivot.position
+        },
+
+        getFiles () {
+            let files = []
+
+            for (let file of this.files) {
+                if (this.showFile(file)) {
+                    file.position = file.playlists[this.currentPlaylist.id][0].pivot.position
+                    files.push(file)
+                }
+            }
+
+            return files
+        },
+
+        async showModal (file) {
+            await this.$store.dispatch('files/setChosenFile', file)
+            this.$root.$emit('bv::show::modal', 'file-modal')
         }
     }
 }
